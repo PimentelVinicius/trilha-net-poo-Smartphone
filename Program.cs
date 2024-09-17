e20318a0aea8c00ed300de3493b482887b24f88a
Vinicius Pimentel <vinicius.pimentel.furtado@hotmail.com>
2024-09-17 20:43:36 -0300

    Alterações propostas realizadas todas com sucesso.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia:");
+
+Nokia SmartNokia = new Nokia(numero: "1234", modelo: "Nokia 1" , imei: "123547894", memoria: 64);
+SmartNokia.Ligar();
+SmartNokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+
+Iphone SmartIphone = new Iphone(numero: "5647", modelo: "Iphone 1" , imei: "22222222", memoria: 128);
+SmartIphone.Ligar();
+SmartIphone.InstalarAplicativo("Telegram");
